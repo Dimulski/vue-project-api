69fab95829e66c45ed6749cf066a32b6f3bcb279
George <george.dimulski@gmail.com>
2019-10-28 18:56:53 +0200

    Initial commit

---
+++ vue-project-api/src/main/java/dimulski/vueprojectapi/VueProjectApiApplication.java
@@ -0,0 +1,12 @@
+package dimulski.vueprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueProjectApiApplication.class, args);
+	}
+}
---
+++ vue-project-api/src/test/java/dimulski/vueprojectapi/VueProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dimulski.vueprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
